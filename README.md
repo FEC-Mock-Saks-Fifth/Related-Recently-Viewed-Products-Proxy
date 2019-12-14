377f41d82a7daa7511f84aa93b28d3e5289c51de
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2019-12-14 10:15:47 -0800

    file clean up, updated the static serve paths to work without modification from team

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Related-Recently-Viewed-Products-Proxy
+# Related-Recently-Viewed-Products
+
+> This repo is for the SaksFifth Clone project proxy server.
+
+## Related Projects
+
+  - https://github.com/FEC-Mock-Saks-Fifth/Product-Review
+  - https://github.com/FEC-Mock-Saks-Fifth/Nav-Search
+  - https://github.com/FEC-Mock-Saks-Fifth/Product-Description
+  - https://github.com/FEC-Mock-Saks-Fifth/Related-Recently-Viewed-Products
+
+## Table of Contents
+
+1. [Usage](#Usage)
+1. [Requirements](#requirements)
+1. [Development](#development)
+
+## Usage
+
+> Create a parent directory to store this repo and all of the companion repos listed above.
+> For each companion repo: run seeding script, run bundling script (if applicable), run server.
+> This proxy functions by serving the bundled code from each companion repo to the front end via index.html in the common directory. It routes all api calls per module back to their origin servers.
+
+## Requirements
+
+An `nvmrc` file is included if using [nvm](https://github.com/creationix/nvm).
+
+- Node 6.13.0
+- etc
+
+## Development
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+npm install -g webpack
+npm install
+npm run start:dev
+```
--- server/index.js
+++ server/index.js
@@ -3,26 +3,25 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const proxy = require('http-proxy-middleware');
 const { routes } = require('../reverseproxyconfig.json');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 /* STATIC FILE SERVES */
-/* use the following routes to serve bundles packaged by companion repos */
 /* static html & css files in THIS proxy server */
 app.use(express.static(path.resolve(__dirname, '../common')));
-/* service bundle locations */
-app.use('/reviews', express.static('/Users/huntertreadaway/code_projects/hack_reactor/hrla33/FEC/Product-Review/client/dist/'))
-app.use('/recs', express.static('/Users/huntertreadaway/code_projects/hack_reactor/hrla33/FEC/Related-Recently-Viewed-Products/client/public/'));
-app.use('/search', express.static('/Users/huntertreadaway/code_projects/hack_reactor/hrla33/FEC/Nav-Search/client/dist/'));
-app.use('/productdetail', express.static('/Users/huntertreadaway/code_projects/hack_reactor/hrla33/FEC/Product-Description/client/dist/'));
 
-/* use this to serve bundles packaged by this program */
-// app.use(express.static(path.resolve(__dirname, '../client/public')));
+/* module bundle paths */
+app.use('/reviews', express.static(path.resolve(__dirname, '../../Product-Review/client/dist/')));
+app.use('/recs', express.static(path.resolve(__dirname, '../../Related-Recently-Viewed-Products/client/public/')));
+app.use('/search', express.static(path.resolve(__dirname ,'../../Nav-Search/client/dist/')));
+app.use('/productdetail', express.static(path.resolve(__dirname, '../../Product-Description/client/dist/')));
 
-/* ROUTE DELEGATION TO EXTERNAL MODULES */
+/* ROUTE DELEGATION TO EXTERNAL MODULES - refer to reverseproxyconfig.json for detailed routes */
 for (route of routes) {
   app.use(route.route,
       proxy({
